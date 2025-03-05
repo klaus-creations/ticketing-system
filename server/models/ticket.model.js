f73f133c6e1920151277f0e84b53421a152b1322
Abush Natanem <abushnatanem@gmail.com>
2025-03-05 22:21:38 +0300

    add the ticket model

---
+++ server/models/ticket.model.js
@@ -0,0 +1,23 @@
+import mongoose, { mongo } from "mongoose";
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["open", "in progress", "closed"],
+      default: "open",
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const ticketModel = mongoose.model("Ticket", ticketSchema);
+
+export default ticketModel;
